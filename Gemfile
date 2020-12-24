0731372036ee5dde27832ebc99c628a33aae9f24
yasufumi-ohtaka <75007267+yasufumi-ohtaka@users.noreply.github.com>
2020-12-24 14:39:12 +0900

    Revert "【依頼】商品出品機能の実装　レビュー"

--- Gemfile
+++ Gemfile
@@ -61,5 +61,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'pry-rails'
-
-gem 'active_hash'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,2 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
-
-  with_options presence: true do
-    validates :image, :name, :info, :price
-  end
-
-  validates :price, numericality: { only_integer: true, message: 'Half-width number' }
-
-  validates_inclusion_of :price, in: 300..9999999, message: 'Out of setting range' do
-  end
-
-  with_options numericality: { other_than: 1, message: 'Select' } do
-    validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id 
-  end
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
 end
